97abf4b38ec2dc2cb32c011fb04314d42424d5c0
shubham <kawaleshubham@outlook.com>
2026-05-23 13:22:24 +0530

    first commit

---
+++ 02-component/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+        <h1>First Page</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident voluptate perspiciatis aperiam ad ut aliquid soluta quis optio tempore accusamus magni magnam nulla, saepe asperiores corporis reprehenderit esse minus corrupti.
+        </p>
+    </div>
+  )
+}
+
+export default App
