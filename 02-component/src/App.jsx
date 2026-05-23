f85346b3a16b8bee0e61200aac35996ba7ec7f66
shubham <kawaleshubham@outlook.com>
2026-05-23 20:30:07 +0530

    componets added

--- 02-component/src/App.jsx
+++ 02-component/src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
+import Card from './component/card'
 
 const App = () => {
   return (
     <div>
-        <h1>First Page</h1>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident voluptate perspiciatis aperiam ad ut aliquid soluta quis optio tempore accusamus magni magnam nulla, saepe asperiores corporis reprehenderit esse minus corrupti.
-        </p>
+      <Card />
     </div>
+
+
   )
 }
 
